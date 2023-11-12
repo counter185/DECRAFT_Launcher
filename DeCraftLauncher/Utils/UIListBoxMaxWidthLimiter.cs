77edadd95aea53ebb3d9dd609d4afc31f85e8c61
counter185 <33550839+counter185@users.noreply.github.com>
2023-11-12 09:30:28 +0100

    maybe, finally fix the long classname layout thing

---
+++ DeCraftLauncher/Utils/UIListBoxMaxWidthLimiter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DeCraftLauncher.Utils
+{
+    public class UIListBoxMaxWidthLimiter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is System.Double)
+            {
+                return (double)value - 40;
+                //todo: get rid of this hack
+            }
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}

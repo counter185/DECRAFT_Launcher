38c49a0cc2f76b567b3a4d162cfcbcf1cc6ee045
counter185 <33550839+counter185@users.noreply.github.com>
2024-07-18 13:53:48 +0200

    fix default applet window size making the game go into 1x scale

--- DeCraftLauncher/Utils/JavaCode.cs
+++ DeCraftLauncher/Utils/JavaCode.cs
@@ -235,7 +235,7 @@ public class AppletWrapper {{
         mainFrame = new JFrame(""DECRAFT Applet Wrapper: {className}"");
         {className} a = new {className}();
         mainFrame.add(a);
-        mainFrame.setSize({jar.windowW} < 0 ? 854 : {jar.windowW}, {jar.windowH} < 0 ? 480 : {jar.windowH});
+        mainFrame.setSize({jar.windowW} < 0 ? 960 : {jar.windowW}, {jar.windowH} < 0 ? 540 : {jar.windowH});
         mainFrame.setVisible(true);
         mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
